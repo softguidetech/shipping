ad0129783714e3d1b57f8cc7ab7300779930f5e2
softguidetech <96677990+softguidetech@users.noreply.github.com>
2024-12-16 16:05:45 +0400

    commit

--- tarco-code/finance_vouchers/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import partner_category
-from . import employee
-from . import cash_voucher
-from . import check_voucher
-from . import request_inbound
-from . import check_inbound
-# from . import employee
